8f7dae75533700e8d1229cefed1c5392b8b11a62
William Lamptey <williamlamp512@gmail.com>
2023-01-04 15:34:31 -0800

    corrections 2.0

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for first_num in range(10):
     for second_num in range(10):
         if first_num < second_num:
             print("{:01d}{:01d}".format(first_num, second_num), end=", "
-                    if first_num < 8 or second_num < 9 else "\n")
+                if first_num < 8 or second_num < 9 else "\n")
