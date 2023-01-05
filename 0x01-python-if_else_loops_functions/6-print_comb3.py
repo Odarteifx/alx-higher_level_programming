e2d2a14a2c8fa4954e3bc3e18e816e5a4bdadc98
William Lamptey <williamlamp512@gmail.com>
2023-01-04 16:12:39 -0800

    error correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-for first_num in range(10):
-    for second_num in range(10):
-        if first_num < second_num:
-            print("{:01d}{:01d}".format(first_num, second_num), end=", "
-                if first_num < 8 or second_num < 9 else "\n")
+for i in range(9):
+    j = i + 1
+    for j in range(j, 10):
+        print("{:d}".format(i), end="")
+        if i == 8 and j == 9:
+            print("{:d}".format(j))
+        else:
+            print("{:d}".format(j), end=", ")
