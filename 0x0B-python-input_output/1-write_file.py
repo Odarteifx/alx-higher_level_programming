e48aca15d3e03f05b6d9282fd97bcc9e2a42fbbf
William Lamptey <williamlamp512@gmail.com>
2023-02-07 14:10:18 -0800

    task correction'

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""Module containing a function to write contents to a file"""
+
+"""Defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
-    """Write content to a file, whther it exists or not."""
-    num_char = 0
-    with open(filename, mode="w", encoding="utf-8") as f:
-        """open and overwrite content to a file"""
-        num_char = f.write(text)
-    return 
+    """Write a string to a UTF8 text file.
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""MOdule containing function to append content to a file."""
+
+"""Defines a file-appending function."""
 
 
 def append_write(filename="", text=""):
-    """Append new content, without overwriting existing one."""
-    num_char = 0
-    with open(filename, mode="a", encoding="utf-8") as f:
-        """use the ``a`` mode to append content"""
-        num_char = f.write(text)
-    return 
+    """Appends a string to the end of a UTF8 text file.
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
