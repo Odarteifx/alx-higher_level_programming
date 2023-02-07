1cdb282ae4a13e6e1d7ac73ef96f6f401ce83c43
William Lamptey <williamlamp512@gmail.com>
2023-02-07 14:06:05 -0800

    correction

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,34 +1,38 @@
 #!/usr/bin/python3
-"""Module containing the ``Student`` class definition.
-"""
+
+"""Defines a class Student."""
 
 
 class Student:
-    """``Student`` class definition.
-    """
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the instance of the class.
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of the instance of the class.
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
         """
-        if attrs is None:
-            return self.__dict__
-        else:
-            new_dict = dict()
-            old_dict = self.__dict__
-            for (key, value) in old_dict.items():
-                if key in attrs:
-                    new_dict[key] = value
-            return new_dict
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
     def reload_from_json(self, json):
-        """Uses the ``json`` dictionary to set the attributes of the instance.
+        """Replace all attributes of the Student.
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
         """
-        self.first_name = json['first_name']
-        self.last_name = json['last_name']
-        self.age = json['age']
+        for k, v in json.items():
+            setattr(self, k, v)
