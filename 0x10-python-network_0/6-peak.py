4a74ae855413edb8a41787c7f9280a08fdaf5902
William Lamptey <williamlamp512@gmail.com>
2023-04-30 02:06:31 -0700

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-"""
-Module to find the hightest number in an array
-"""
+"""function that finds a peak in a list of unsorted integers."""
+
+
+def find_peak_high(arr, l, h):
+    """ Find a peak"""
+    m = l + (h - l)//2
+
+    print("low " + str(l) + " high " + str(h))
+    if (l == h):
+        return arr[l]
+    if ((h - l) == 1):
+        return max(arr[l], arr[h])
+
+    if (arr[m] > arr[m-1] and arr[m] > arr[m+1]):
+        return arr[m]
+    elif (arr[m] <  arr[m + 1]):
+        return find_peak_high(arr, m + 1, h)
+    else:
+        return find_peak_high(arr, l, m - 1)
 
 
 def find_peak(list_of_integers):
-        """
-            Method to find hightest number in array
-                """
-                    if bool(list_of_integers) is False:
-                                return None
-                                list_of_integers.sort()
-                                    return list_of_integers[-1]
+    """To find peak"""
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+    return find_peak_high(list_of_integers, 0, (len(list_of_integers) - 1))
